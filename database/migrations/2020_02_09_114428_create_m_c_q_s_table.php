05c14a2c80da239e09eba3f40c5ed59354084b0b
Abdulaziz Alsaeed <Abdulaziz.isd@gmail.com>
2020-03-18 02:02:16 +0300

    create mcqs

---
+++ database/migrations/2020_02_09_114428_create_m_c_q_s_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMCQSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_c_q_s', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text ('question');
+            $table->integer('chapter_no');
+            $table->integer('mark');
+            $table->text('correct_answer');
+            $table->text('option1');
+            $table->text('option2');
+            $table->text('option3');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_c_q_s');
+    }
+}
